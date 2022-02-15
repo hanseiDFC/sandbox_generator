9d1886c794b62039adec53aabdf1704db521fb9a
minpeter <kali2005611@gmail.com>
2022-02-15 13:30:06 +0900

    /remove and /create fin

--- main.py
+++ main.py
@@ -1,24 +1,69 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+import atexit
 import docker
 
 
 app = Flask(__name__)
+client = docker.from_env()
+
+used_port = []
+used_sandbox = []
+
+
+def random_port():
+    import random
+    port = random.randint(9000, 49151)
+    if port not in used_port:
+        used_port.append(port)
+        return port
+    else:
+        return random_port()
+
+
+def remove_sandbox(sandbox_id):
+    sandbox = client.containers.get(sandbox_id)
+    sandbox.stop()
+    sandbox.remove()
+    print(f"{sandbox_id} is removed.")
 
 
 @app.route('/')
 def home():
-    return 'This api is the api that creates the sandbox of hansei wargame.'
+    return jsonify({"massage": 'This api is the api that creates the sandbox of hansei wargame.'})
 
 
-@app.route('/create')
+@app.route('/create', methods=['GET'])
 def create():
-    sandbox_port = 23456
+    sandbox_port = random_port()
     user_name = "root"
     user_password = "root"
-    client = docker.from_env()
-    client.containers.run("sshd", detach=True, ports={22: sandbox_port})
-    return jsonify({"name": user_name, "password": user_password, "port": sandbox_port})
+
+    sandbox_id = client.containers.run(
+        "sshd", detach=True, ports={22: sandbox_port}).id[:12]
+    return_msg = {"massage": 'The sandbox is created.', "sandbox_port": sandbox_port,
+                  "user_name": user_name, "sandbox_id": sandbox_id, "user_password": user_password}
+    used_sandbox.append(return_msg)
+
+    # 나중에 유저 토큰 추가
+    return jsonify(return_msg)
+
+
+@app.route('/remove', methods=['POST'])
+def remove():
+    id = request.json['id']
+    for sandbox in used_sandbox:
+        if sandbox['sandbox_id'] == id:
+            remove_sandbox(sandbox['sandbox_id'])
+            used_sandbox.remove(sandbox)
+            return jsonify({"massage": 'The sandbox is removed.'})
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000', debug=True)
+    app.run(host='0.0.0.0', port='5000', debug=False)
+
+
+@atexit.register
+def sandbox_cleanup():
+    for sandbox in used_sandbox:
+        remove_sandbox(sandbox['sandbox_id'])
+    print("All sandbox are removed.")
